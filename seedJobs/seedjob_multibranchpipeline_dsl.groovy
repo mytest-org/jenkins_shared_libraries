b9c51c240b57b1e1da3f09fde875a6e5597cb26c
Umair A. Shahid <umairr@gmail.com>
2019-10-01 14:36:04 +0500

    Adding seed job with multi-branch pipeline

--- seedJobs/seedjob_multibranchpipeline_dsl.groovy
+++ seedJobs/seedjob_multibranchpipeline_dsl.groovy
@@ -2,69 +2,23 @@ projectName = 'eureka-server'
 scmRepositoryUrl = 'https://gitlab.mynisum.com/devops/eureka-server.git'
 scmCredentialsId = 'gitlab_cred'
 
-multibranchPipelineJob("${projectName}") {
+multibranchPipelineJob("GAP/${projectName}_PR_Build") {
     branchSources {
-        branchSource {
-            source {
-                git {
-                    id('dd847135-8391-4f66-a54c-7f8781dc3119')
-                    remote("${scmRepositoryUrl}")
-                    credentialsId("${scmCredentialsId}")
-                }
-            }
+//        branchSource {
+//            source {
+        git {
+            id('dd847135-8391-4f66-a54c-7f8781dc3119')
+            remote("${scmRepositoryUrl}")
+            credentialsId("${scmCredentialsId}")
+            includes('*')
+            excludes('')
         }
+        //           }
+        //       }
     }
     factory {
-        workflowMultiBranchProjectFactory {
+        workflowBranchProjectFactory {
             scriptPath('Jenkinsfile')
         }
     }
 }
-
-
-/*
-multibranchPipelineJob("${projectName}") {
-    displayName "${projectName}"
-    description "multi-branch pipeline job for ${projectName}"
-    configure {
-        it / sources / 'data' / 'jenkins.branch.BranchSource' << {
-            source(class: 'jenkins.plugins.git.GitSCMSource') {
-                id(uuid)
-                remote("${scmRepositoryUrl}")
-                credentialsId("${scmCredentialsId}")
-                includes('*')
-                excludes('')
-                ignoreOnPushNotifications('false')
-                traits {
-                    'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
-                }
-            }
-            // default strategy when sourcing from a branch
-            strategy(class: "jenkins.branch.NamedExceptionsBranchPropertyStrategy") {
-                defaultProperties(class: "java.util.Arrays\$ArrayList") {
-                    a(class: "jenkins.branch.BranchProperty-array") {
-                        // don't trigger builds
-                        "jenkins.branch.NoTriggerBranchProperty"()
-                    }
-                }
-                // exceptions to the default strategy
-                namedExceptions(class: "java.util.Arrays\$ArrayList") {
-                    a(class: "jenkins.branch.NamedExceptionsBranchPropertyStrategy\$Named-array") {
-                        "jenkins.branch.NamedExceptionsBranchPropertyStrategy_-Named"() {
-                            // for the brach named `master` trigger builds
-                            //   (this is default behaviour if no branch properties are specified)
-                            props(class: "empty-list")
-                            name("master")
-                        }
-                    }
-                }
-            }
-        }
-        // customise the branch project factory
-        it / factory(class: "org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory") << {
-            // pipeline jobs will have their script path set to `pipelines/customPipeline.groovy`
-            scriptPath("Jenkinsfile")
-        }
-    }
-}
-*/
