b9fd16da0b755aede73b31c91a8caa79398fe23b
Umair A. Shahid <umairr@gmail.com>
2019-10-02 17:06:49 +0500

    Changing repo url from gitlab to github

--- seedJobs/seedjob_multibranchpipeline_dsl.groovy
+++ seedJobs/seedjob_multibranchpipeline_dsl.groovy
@@ -1,13 +1,14 @@
 projectName = 'eureka-server'
-scmRepositoryUrl = 'https://gitlab.mynisum.com/devops/eureka-server.git'
-scmCredentialsId = 'gitlab_cred'
+//scmRepositoryUrl = 'https://gitlab.mynisum.com/devops/eureka-server.git'
+scmRepositoryUrl = 'https://github.com/mytest-org/eureka-server.git'
+//scmCredentialsId = 'gitlab_cred'
 
 multibranchPipelineJob("GAP/${projectName}_PR_Build") {
     branchSources {
         git {
             id('dd847135-8391-4f66-a54c-7f8781dc3119')
             remote("${scmRepositoryUrl}")
-            credentialsId("${scmCredentialsId}")
+//            credentialsId("${scmCredentialsId}")
             includes('*')
             excludes('')
         }
