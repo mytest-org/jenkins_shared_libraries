227fe6a3568d56ee59d2eb01043c32b5bf7bb426
fqadeer <fqadeer@nisum.com>
2019-10-07 18:33:00 +0500

    delete ExecuteGradle file and add Build groovy file

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,12 +1,8 @@
-import org.gap.build.BuildGradle
 import org.gap.build.ExecuteBuild
 import org.gap.ioc.ContextRegistry
 
 def call(String tool, args) {
     ContextRegistry.registerDefaultContext(this)
-
-//    def buildGradle = new BuildGradle(goals)
-//    buildGradle.build()
 
     def executeBuild = new ExecuteBuild(tool, args)
     executeBuild.build()
