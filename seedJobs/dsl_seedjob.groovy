1ef0c34ffa003722c92dbc878a1eeb6ab02318dc
Umair A. Shahid <umairr@gmail.com>
2019-10-07 19:20:07 +0500

    Using shared library in seed job groovy file

---
+++ seedJobs/dsl_seedjob.groovy
@@ -0,0 +1,16 @@
+import org.gap.seedjob.GithubMultiBranch
+
+def Pipeline = new GithubMultiBranch(
+        projectName: 'eureka-server',
+        pipelineName: "${projectName_PR_Build}",
+        scmRepositoryOwner: 'mytest-org',
+        scmRepository: 'eureka-server',
+        scmCredentialsId: '617bab8b-39fe-43f0-a2a6-a82628d85442',
+        scmApiUrl: 'https://api.github.com',
+        buildForkPRHead: 'false',
+        buildForkPRMerge: 'false',
+        buildOriginBranch: 'false',
+        buildOriginBranchWithPR: 'false',
+        buildOriginPRHead: 'false',
+        buildOriginPRMerge: 'true'
+).build(this)
