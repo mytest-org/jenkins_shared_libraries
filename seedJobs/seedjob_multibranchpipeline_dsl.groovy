5e5ddb8132480bbcbf01540937a6a3b96244c940
Umair A. Shahid <umairr@gmail.com>
2019-10-07 19:21:32 +0500

    old seed job dsl file

--- seedJobs/seedjob_multibranchpipeline_dsl.groovy
+++ seedJobs/seedjob_multibranchpipeline_dsl.groovy
@@ -1,18 +1,25 @@
 projectName = 'eureka-server'
-//scmRepositoryUrl = 'https://gitlab.mynisum.com/devops/eureka-server.git'
 scmRepositoryUrl = 'https://github.com/mytest-org/eureka-server.git'
-//scmCredentialsId = 'gitlab_cred'
+scmCredentialsId = 'GIT_CREDENTIALS'
+scmApiCredentialsId = '617bab8b-39fe-43f0-a2a6-a82628d85442'
 
 multibranchPipelineJob("GAP/${projectName}_PR_Build") {
     branchSources {
-        git {
-            id('dd847135-8391-4f66-a54c-7f8781dc3119')
-            remote("${scmRepositoryUrl}")
-//            credentialsId("${scmCredentialsId}")
-            includes('*')
-            excludes('')
+        github {
+            id("f04e2d72-fa29-4cd9-8de3-5b28243675fc")
+            scanCredentialsId("${scmApiCredentialsId}")
+            //        checkoutCredentialsId("${scmCredentialsId}")
+            apiUri("https://api.github.com")
+            repoOwner("mytest-org")
+            repository("eureka-server")
+            buildForkPRHead(false)
+            buildForkPRMerge(false)
+            buildOriginBranch(false)
+            buildOriginBranchWithPR(false)
+            buildOriginPRHead(false)
+            buildOriginPRMerge(true)
         }
-    }
+    }  // end of branchSources
     factory {
         workflowBranchProjectFactory {
             scriptPath('Jenkinsfile')
