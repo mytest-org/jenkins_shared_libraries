ef5a6d16c33fa956e3bd500353f1306a0ce7deab
fqadeer <fqadeer@nisum.com>
2019-10-07 18:20:08 +0500

    update methods

---
+++ src/org/gap/build/ExecuteBuild.groovy
@@ -0,0 +1,26 @@
+package org.gap.build
+
+import org.gap.IStepExecutor
+import org.gap.ioc.ContextRegistry
+
+class ExecuteBuild {
+
+    private String _tool
+    private String _args
+
+    BuildGradle(String tool, String args) {
+        _tool = tool
+        _args = args
+
+    }
+
+    void build() {
+        IStepExecutor steps = ContextRegistry.getContext().getStepExecutor()
+
+        int returnStatus = steps.sh("${this._tool} ${this._args}")
+        if (returnStatus != 0) {
+            steps.error("Errors while building with ${this._tool} with arguments ${this._args}.")
+        }
+    }
+
+}
--- vars/ExecuteGradle.groovy
+++ vars/ExecuteGradle.groovy
@@ -1,9 +1,14 @@
 import org.gap.build.BuildGradle
+import org.gap.build.ExecuteBuild
 import org.gap.ioc.ContextRegistry
 
-def call(String goals) {
+def call(String tool, args) {
     ContextRegistry.registerDefaultContext(this)
 
-    def buildGradle = new BuildGradle(goals)
-    buildGradle.build()
+//    def buildGradle = new BuildGradle(goals)
+//    buildGradle.build()
+
+    def executeBuild = new ExecuteBuild(tool, args)
+    executeBuild.build()
+
 }
